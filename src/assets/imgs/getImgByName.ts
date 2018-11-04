845c650acb6a5ffced9b0b0acb0d2e7c7648d3b1
Dmitry <30289917+DmitryUsik@users.noreply.github.com>
2018-11-04 11:43:04 +0200

    Update getImgByName.ts

--- src/assets/imgs/getImgByName.ts
+++ src/assets/imgs/getImgByName.ts
@@ -12,16 +12,16 @@ export type ImgName = 'logo_primary'
     | 'back_arrow'
 
 const imgs: OrderedMap<String, any> = OrderedMap({
-        ['logo_primary']: require('./logo_primary.png'),
-        ['logo_white']: require('./logo_white.png'),
-        ['flag']: require('./flag.png'),
-        ['region']: require('./region.png'),
-        ['africa']: require('./africa.png'),
-        ['americas']: require('./america.png'),
-        ['asia']: require('./asia.png'),
-        ['europe']: require('./europe.png'),
-        ['oceania']: require('./oceania.png'),
-        ['back_arrow']: require('./back_arrow.png')
+        logo_primary: require('./logo_primary.png'),
+        logo_white: require('./logo_white.png'),
+        flag: require('./flag.png'),
+        region: require('./region.png'),
+        africa: require('./africa.png'),
+        americas: require('./america.png'),
+        asia: require('./asia.png'),
+        europe: require('./europe.png'),
+        oceania: require('./oceania.png'),
+        back_arrow: require('./back_arrow.png')
     }
 )
 
